403ee6f3ba229e585f7489c9a12c2249579f2ad3
Hamish Currie <hamish@currie.com.au>
2021-11-30 07:25:20 +0000

    doing a tutorial from https://blog.logrocket.com/using-typescript-with-react-tutorial-examples/

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FirstComponent from "./components/FirstComponent"
+import UserComponent from './components/UserComponent';
 function App() {
   return (
     <div className="App">
       <FirstComponent />
+      <UserComponent name="John Doe" age={26} address="hi there lane" dob={new Date()} />
     </div>
   );
 }
